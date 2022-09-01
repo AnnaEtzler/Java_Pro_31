6a7744ba89923ac2722150f97bb5c8ddc8bcf394
Anna <anna.piskunova@web.de>
2022-09-01 08:51:48 +0200

    homework alg lesson 4

--- src/main/java/algorithms/lesson4/homework4/Homework_2.java
+++ src/main/java/algorithms/lesson4/homework4/Homework_2.java
@@ -4,5 +4,41 @@ package algorithms.lesson4.homework4;
 //Если какая-то строка больше чем другая, то остаток переписываем в результат.
 //Метод аналогичен слиянию массивов в merge sort, но без учета сравнения элементов.
 
+import java.util.Arrays;
+
 public class Homework_2 {
+    public static void main(String[] args) {
+        String s1 = "abcde";
+        String s2 = "dgk";
+
+        String[] result = new String[s1.length() + s2.length()]; //a d b g c k d e
+        int count1 = 0;
+        int count2 = 0;
+        int countResult = 0;
+        while (countResult < result.length) {
+            if (count1 == s1.length()) {
+                result[countResult] = String.valueOf(s2.charAt(count2));
+                count2++;
+                countResult++;
+                continue;
+            }
+            if (count2 == s2.length()) {
+                result[countResult] = String.valueOf(s1.charAt(count1));
+                count1++;
+                countResult++;
+                continue;
+            }
+            if (count1 <= count2) {
+                result[countResult] = String.valueOf(s1.charAt(count1));
+                count1++;
+            } else {
+                result[countResult] = String.valueOf(s2.charAt(count2));
+                count2++;
+            }
+            countResult++;
+        }
+
+        System.out.println(Arrays.toString(result));
+    }
+
 }
--- src/main/java/algorithms/lesson4/homework4/Homework_3.java
+++ src/main/java/algorithms/lesson4/homework4/Homework_3.java
@@ -1,4 +1,5 @@
 package algorithms.lesson4.homework4;
+
 //Дан отсортированный по возрастанию массив, все элементы этого массива уникальные.
 //Дано целое число n , написать метод, который вернет n-й отсутствующий элемент в массиве.
 //Например дан массив: [4,7,9,10,14]
@@ -6,4 +7,36 @@ package algorithms.lesson4.homework4;
 //n = 2, вернуть второй отсутствующий элемент : ответ 6
 //n = 3, вернуть третий отсутствующий элемент : ответ 8
 public class Homework_3 {
+    public static void main(String[] args) {
+        int[] array = new int[]{4, 7, 9, 10, 14};
+        int n = 7;
+        int result = missingElement(array, n);
+        System.out.println(result);
+
+    }
+
+    private static int missingElement(int[] array, int n) {
+        if (n == 0) {
+            return array[0] - 1;
+        }
+        if (n > array.length) {
+            return array[0] + n + array.length - 1;
+        }
+
+        int count = 1;
+        for (int i = 0; i < array.length - 1; i++) {
+            int difference = array[i + 1] - array[i];
+            for (int j = 1; j < difference; j++) {
+                if (count == n) {
+                    return array[0] + count + i;
+                }
+
+                count++;
+            }
+
+
+        }
+        return 0;
+    }
+
 }
