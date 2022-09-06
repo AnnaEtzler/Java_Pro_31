ae1293a468633a659505b5b00cf46e0d2a694d4f
Viktor Zorya <anna0795@bk.ru>
2022-09-06 02:21:24 +0200

    commit homework 10

---
+++ src/main/java/lesson10/homework/Homework10.java
@@ -0,0 +1,24 @@
+package lesson10.homework;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Homework10 {
+    public static void main(String[] args) {
+        String [] country = new String[]{"Andorra", "Belize", "Cayman", "France", "Argentina", "Cuba", "Sweden"};
+        String [] word = new String[]{"Andorra", "Canada", "First", "candy", "Argentina", "wood", "parrot", "cat", "Alan", "Cuba", "Finland", "Axelrod" , "Avangard", "Cuba"};
+        Set<String> setWord = new HashSet<String>(List.of(word));
+        Set<String> setCountry = new HashSet<String>(List.of(country));
+        setWord.retainAll(setCountry);
+
+        for (String s : setWord){
+            if(!s.startsWith("A")){
+                setWord.remove(s);
+            }
+
+        }
+        System.out.println(setWord);
+
+    }
+}
