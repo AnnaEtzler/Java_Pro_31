9f8fad21ef7510e48080d409cacb472921ed88f5
Anna <anna.piskunova@web.de>
2022-10-04 13:28:15 +0200

    homework 17

--- src/main/java/lesson17/Homework_17.java
+++ src/main/java/lesson17/Homework_17.java
@@ -1,16 +1,23 @@
 package lesson17;
 
-// напишите метод для кодирования строк. Строка состоит только из маленьких латинских букв. Пример: "aaaabbcccdaaa" -> "4a2b3c1d3a"
-//* напишите метод для декодирования такой строки
-//* напишите метод, который вернет true если в массиве присутствуют два числа, произведение которых даст нужное число. Пример [5,7,12, 2] , 10 -> true
-//public static boolean test(int [] a, int b) (о
+
+
+
 public class Homework_17 {
     public static void main(String[] args) {
         String s = "aaaabbcccdaaa";
         String result = encode(s);
         System.out.println(result);
+        String result2 = decode(result);
+        System.out.println(result2);
+        int [] array = {5,7,12, 4,3};
+        int number = 12;
+        System.out.println(test(array, number));
+
     }
 
+
+    // напишите метод для кодирования строк. Строка состоит только из маленьких латинских букв. Пример: "aaaabbcccdaaa" -> "4a2b3c1d3a"
     private static String encode(String s) {
         char[] chars = s.toCharArray();
         StringBuilder sbResult = new StringBuilder();
@@ -30,4 +37,37 @@ public class Homework_17 {
         }
         return sbResult.toString();
     }
+
+    //* напишите метод для декодирования такой строки
+    private static String decode(String s) {
+        char[] array = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i += 2) {
+            String zahl = String.valueOf(array[i]);
+            int zahlResult = Integer.parseInt(zahl);
+            String str = String.valueOf(array[i + 1]);
+            for (int j = 0; j < zahlResult; j++) {
+                sb.append(str);
+            }
+
+        }
+
+        return sb.toString();
+
+    }
+
+    //* напишите метод, который вернет true если в массиве присутствуют два числа, произведение которых даст нужное число. Пример [5,7,12, 2] , 10 -> true
+    public static boolean test(int [] array, int number) {
+        for(int i = 0; i < array.length; i++){
+            for(int j = i+1; j < array.length; j++){
+                if(array[i]*array[j] == number){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+
 }
